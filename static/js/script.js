b9098e549e20fa880405b4130a66e9a5a3b2de3a
Kushagra <patidarkush751@gmail.com>
2024-11-10 17:04:46 +0530

    Initial commit

---
+++ static/js/script.js
@@ -0,0 +1,4 @@
+// script.js
+document.getElementById('clickButton').addEventListener('click', function() {
+    document.getElementById('message').innerText = 'Hello, You clicked the button!';
+});
